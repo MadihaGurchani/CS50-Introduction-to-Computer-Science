2b4d0a25f5bcd5c8915e2ccb65d50b039e6a5559
MadihaGurchani <MadihaGurchani@users.noreply.github.com>
2020-04-29 19:35:32 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,26 +4,30 @@
 int get_height(string prompt);
 int main(void)
 {
+    //Call get_height function to get value
     int height = get_height("Height: ");
-
+    //Make pyramid with value entered
     for (int row = 0; row < height + 1; row++)
     {
-       if (row > 0)
+        //iterate over each row
+        if (row > 0)
         {
-        int sign = height - row;
-        printf("%.*s", sign , "             ");
+            int sign = height - row;
+            printf("%.*s", sign, "             ");
         }
-        for(int colum = 0 ; colum < row ;colum++)
+        //iterate over each colum
+        for (int colum = 0 ; colum < row ; colum++)
         {
             printf("#");
         }
+        //Move to next line
         printf("\n");
     }
 }
 
 
 
-//Prompt user for input
+//Prompt user for value
 int get_height(string prompt)
 {
     int number;
@@ -31,6 +35,7 @@ int get_height(string prompt)
     {
         number = get_int("%s", prompt);
     }
+    //Run loop only if number is between 1 and 8 inclusive
     while (number < 1 || number > 8);
     return number;
 }
