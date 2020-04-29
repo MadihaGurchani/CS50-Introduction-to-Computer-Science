900ccd60387460fa6b6e99731d15981936b323a4
MadihaGurchani <MadihaGurchani@users.noreply.github.com>
2020-04-29 19:59:46 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -6,40 +6,48 @@ float get_change(string prompt);
 int main(void)
 {
     int coins = 0;
-    int change = round(get_change("Change: ")*100);
-    //Procedure for counting coins
+    //Convert the user input to cents
+    int change = round(get_change("Change: ") * 100);
+    //Check if user input is greater than or equal to 25 cents
     if (change > 25 || change == 25)
     {
         do
         {
             change = change - 25;
             coins++;
-        }while (change > 24);
+        }
+        while (change > 24);
     }
+    ////Check if user input is greater than or equal to 25 cents
     if (change > 10 || change == 10)
     {
         do
         {
             change = change - 10;
             coins++;
-        }while (change > 9);
+        }
+        while (change > 9);
     }
+    //Check if user input is greater than or equal to 5 cents
     if (change > 5 || change == 5)
     {
         do
         {
             change = change - 5;
             coins++;
-        }while (change > 4);
+        }
+        while (change > 4);
     }
-    if (change > 1 || change == 1 )
+    //Check if user input is greater than or equal to 1 cents
+    if (change > 1 || change == 1)
     {
-       do
-       {
+        do
+        {
 
-         change = change - 1;
-         coins++;
-       }while ( change > 0);
+            change = change - 1;
+            coins++;
+        }
+        while (change > 0);
     }
     printf("%i\n", coins);
 }
@@ -50,8 +58,11 @@ float get_change(string prompt)
     float number;
     do
     {
-        number = get_float("%s",prompt);
+        number = get_float("%s", prompt);
     }
-    while(number < 0);
+    //Run loop as long as number is less than 0
+    while (number < 0);
     return number;
 }
+
+
