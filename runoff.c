53b0bae0e42878f77280afc09794535dbd6fcdf1
MadihaGurchani <MadihaGurchani@users.noreply.github.com>
2020-04-30 11:20:55 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -151,7 +151,7 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-             if (candidates[preferences[i][j]].eliminated == false )
+            if (candidates[preferences[i][j]].eliminated == false)
                 {
                      candidates[preferences[i][j]].votes++;
                      break;
