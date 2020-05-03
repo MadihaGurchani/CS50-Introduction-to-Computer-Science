d3768bc7867e08cd9b4ec4d4f84020f164cb73fb
MadihaGurchani <MadihaGurchani@users.noreply.github.com>
2020-05-03 13:32:55 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -1,6 +1,11 @@
 // Implements a dictionary's functionality
 
+#include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -12,26 +17,47 @@ typedef struct node
 }
 node;
 
-// Number of buckets in hash table
-const unsigned int N = 1;
-
+// Number of buckets in hash table 2^16
+const unsigned int N = 65536;
 // Hash table
 node *table[N];
-
-typedef struct node *node_make;
-node_make node_producer(const char *key);
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
     // TODO
+    //lowercase word
+    char lower_case[LENGTH + 1];
+    //change word to all lower case letters
+    for (int i = 0; i < strlen(word); i++)
+    {
+        lower_case[i] = tolower(word[i]);
+    }
+    //Find index of word
+    int index = hash(lower_case);
+    //Set pointer to the correct head in the hash table
+    node *pointer = table[index];
+    //Traverse the linked list
+    while (pointer != NULL)
+    {
+        //Ignoring case, check if word matches
+        if(strcasecmp(word, pointer->word) == 0)
+        {
+            return true;
+        }
+        pointer = pointer->next;
+    }
     return false;
 }
-
+//* https://www.reddit.com/r/cs50/comments/1x6vc8/pset6_trie_vs_hashtable/cf9nlkn/
 // Hashes word to a number
-unsigned int hash(const char *word)
+unsigned int hash(const char *needs_hashing)
 {
     // TODO
+    unsigned int hash = 0;
+    for (int i=0, n=strlen(needs_hashing); i<n; i++)
+        hash = (hash << 2) ^ needs_hashing[i];
+    return hash % N;
     return 0;
 }
 
@@ -39,23 +65,36 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-     //check if dictionary file opened successfully
-     FILE *file = fopen(dictionary,"r");
-     if (file == NULL)
-     {
-        return false;
-     }
-    //
-    char term[length + 1];
-    while(fscanf(file, %s, term) != EOF)
+    //OPEN Dictionary file successfully
+    FILE *file_pointer =  fopen(dictionary, "r");
+    if (file_pointer == NULL)
     {
-        node_make node = node_producer(term);
-        hash(word)
-
-
-    word++
+        printf("Could not open file\n");
+        return false;
     }
-    fclose(file);
+    //create an array to store words
+    char word[LENGTH + 1];
+    //Red through the file
+    while (fscanf(file_pointer,"%s",word) != EOF)
+    {
+       fscanf(file_pointer,"%s",word);
+       //allocate memory for node
+       node *n = malloc(sizeof(node));
+       if (n == NULL)
+       {
+          unload();
+          return false;
+
+       }
+       //insert word in node
+       strcpy(n->word,word);
+       //get index for insertion
+       int index = hash(n->word);
+       //Insert in the correct linked list
+       n->next = table[index];
+       table[index] = n;
+    }
+    fclose(file_pointer);
     return true;
 }
 
@@ -63,23 +102,42 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    //iterating over the heads in the hash table
+    int total = 0;
+    for (int i = 0; i < N; i++)
+    {
+     // set pointer to head of list
+     node *pointer = table[i];
+     // traverse list
+     while (pointer != NULL)
+     {
+        total++;
+        pointer = pointer->next;
+     }
+    }
+    return total;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
     // TODO
+    //iterate over the heads in the hash table
+    for (int i = 0; i < N; i++)
+    {
+        node *pointer = table[i];
+        //traverse the linked list
+        while (pointer != NULL)
+        {
+           node *tmp = pointer;
+           pointer = pointer->next;
+           free(tmp);
+        }
+        free(pointer);
+    }
     return false;
 }
 //Node making machine
-node_make node_producer
-{
-    node_make node = malloc(sizeof(struct(node)));
-    strcpy(node->word, key);
-    node->next = NULL;
-    return node;
-}
 
 
 
