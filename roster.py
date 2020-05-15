47952441c805bea0d0661cfe2af38e3e923fb11f
MadihaGurchani <MadihaGurchani@users.noreply.github.com>
2020-05-15 19:45:09 +0000

    automated commit by check50 [check50=True]

--- roster.py
+++ roster.py
@@ -5,31 +5,21 @@ from sys import argv, exit
 
 def main():
      # Check if correct number  of command-line arguments and string given
-     if not (len(argv) == 2 and isinstance(argv[1],str)):
-         print("Incorrect input")
-         exit(1)
+     if len(argv) < 2:
+        print("Dear Muggle, you have inserted an invalid input")
+        exit()
 
-     # Check if valid house entered
-     houses = ["ravenclaw", "gryffindor", "hufflepuff", "slytherin"]
-     # Convert house to lower caps for case insensitivity
-     house = argv[1].lower()
-     if house not in houses:
-        print("Dear Muggle, you have inserted an invalid house name")
-        exit(1)
-
-     # Open database
-     db = cs50.SQL("sqlite:///students.db")
-     # Order correctly
+     # open the database in a variable and then execute a query that list all the people from a particular house in alphabetical order
+     db = SQL("sqlite:///students.db")
      students = db.execute("SELECT * FROM students WHERE house = (?) ORDER BY last", argv[1])
 
-     # Print data correctly
-     for row in students:
-        if row["middle"] == None:
-            print(f"{students['first']} {students['last']}, born {students['birth']}")
-        else:
-            print(f"{students['first']} {students['middle']} {students['last']}, born {students['birth']}")
+     # print each person showing their information and their middle name if they have one
+     for student in students:
+         if student['middle'] == None:
+             print(f"{student['first']} {student['last']}, born {student['birth']}")
+         else:
+            print(f"{student['first']} {student['middle']} {student['last']}, born {student['birth']}")
+
      exit(0)
 
-if __name__ == "__main__":
-    main()
-
+main()
